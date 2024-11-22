aec213c35db49201d546a7d6524487f5fc44a001
narashima <narashimagopi123@gmail.com>
2024-11-22 14:38:00 +0530

    added extra input fields

--- gpa.js
+++ gpa.js
@@ -7,6 +7,9 @@ function calculateGPA() {
   const s6 = parseFloat(document.getElementById("s6").value);
   const s7 = parseFloat(document.getElementById("s7").value);
   const s8 = parseFloat(document.getElementById("s8").value);
+  const s9 = parseFloat(document.getElementById("s9").value);
+  const s10 = parseFloat(document.getElementById("s10").value);
+  const s11 = parseFloat(document.getElementById("s11").value);
   const c1 = parseFloat(document.getElementById("c1").value);
   const c2 = parseFloat(document.getElementById("c2").value);
   const c3 = parseFloat(document.getElementById("c3").value);
@@ -15,8 +18,11 @@ function calculateGPA() {
   const c6 = parseFloat(document.getElementById("c6").value);
   const c7 = parseFloat(document.getElementById("c7").value);
   const c8 = parseFloat(document.getElementById("c8").value);
-  const sub = [s1,s2,s3,s4,s5,s6,s7,s8];
-  const credit = [c1,c2,c3,c4,c5,c6,c7,c8];
+  const c9 = parseFloat(document.getElementById("c9").value);
+  const c10 = parseFloat(document.getElementById("c10").value);
+  const c11 = parseFloat(document.getElementById("c11").value);
+  const sub = [s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11];
+  const credit = [c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11];
   let newSub = [];
   let newCredit = [];
   sub.filter(Boolean).forEach(s => {
